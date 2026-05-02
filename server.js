cea41320c0d398871223e768ee50e0e3e660132e
shreya-24-afk <shreyatelsang02@gmail.com>
2026-05-02 23:11:13 +0530

    fixed port for render

--- server.js
+++ server.js
@@ -376,6 +376,8 @@ app.get("/appointments", async (req, res) => {
 });
 
 // ================= SERVER =================
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running");
 });
