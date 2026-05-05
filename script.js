28a30444fc9254ef061e66c1fb71f81e3c7a7dd2
shreya-24-afk <shreyatelsang02@gmail.com>
2026-05-05 12:35:24 +0530

    final deployment setup

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const BASE_URL =
   window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1"
     ? "http://localhost:5000"
-    : "https://YOUR-BACKEND.onrender.com"; 
+    : "https://medico-appointment.onrender.com";
+
 
 
 // ================= FORM SWITCH =================
--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cors({
   origin: [
     "http://localhost:5500",
     "http://127.0.0.1:5500",
-    "https://YOUR-FRONTEND.github.io"
+    "https://shreya-24-afk.github.io/Medico-Appointment/"
   ],
   credentials: true
 }));
